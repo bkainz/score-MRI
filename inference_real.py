763c859e3d2005e68fa74cc8048596866a7bded2
Bernhard Kainz <bernhard.kainz1@gmail.com>
2023-09-28 07:31:55 +0000

    experimenting

--- inference_real.py
+++ inference_real.py
@@ -1,3 +1,4 @@
+from typing import List, Optional
 from pathlib import Path
 from models import utils as mutils
 from sde_lib import VESDE
@@ -14,6 +15,163 @@ from models.ema import ExponentialMovingAverage
 import matplotlib.pyplot as plt
 import importlib
 import argparse
+import h5py
+import torchvision.transforms as T
+import torchvision
+import imageio
+
+def fft2c_new(data: torch.Tensor, norm: str = "ortho") -> torch.Tensor:
+    """
+    Apply centered 2 dimensional Fast Fourier Transform.
+
+    Args:
+        data: Complex valued input data containing at least 3 dimensions:
+            dimensions -3 & -2 are spatial dimensions and dimension -1 has size
+            2. All other dimensions are assumed to be batch dimensions.
+        norm: Normalization mode. See ``torch.fft.fft``.
+
+    Returns:
+        The FFT of the input.
+    """
+    if not data.shape[-1] == 2:
+        raise ValueError("Tensor does not have separate complex dim.")
+
+    data = ifftshift(data, dim=[-3, -2])
+    data = torch.view_as_real(
+        torch.fft.fftn(  # type: ignore
+            torch.view_as_complex(data), dim=(-2, -1), norm=norm
+        )
+    )
+    data = fftshift(data, dim=[-3, -2])
+
+    return data
+
+
+def ifft2c_new(data: torch.Tensor, norm: str = "ortho") -> torch.Tensor:
+    """
+    Apply centered 2-dimensional Inverse Fast Fourier Transform.
+
+    Args:
+        data: Complex valued input data containing at least 3 dimensions:
+            dimensions -3 & -2 are spatial dimensions and dimension -1 has size
+            2. All other dimensions are assumed to be batch dimensions.
+        norm: Normalization mode. See ``torch.fft.ifft``.
+
+    Returns:
+        The IFFT of the input.
+    """
+    if not data.shape[-1] == 2:
+        raise ValueError("Tensor does not have separate complex dim.")
+
+    data = ifftshift(data, dim=[-3, -2])
+    data = torch.view_as_real(
+        torch.fft.ifftn(  # type: ignore
+            torch.view_as_complex(data), dim=(-2, -1), norm=norm
+        )
+    )
+    data = fftshift(data, dim=[-3, -2])
+
+    return data
+
+
+# Helper functions
+
+
+def roll_one_dim(x: torch.Tensor, shift: int, dim: int) -> torch.Tensor:
+    """
+    Similar to roll but for only one dim.
+
+    Args:
+        x: A PyTorch tensor.
+        shift: Amount to roll.
+        dim: Which dimension to roll.
+
+    Returns:
+        Rolled version of x.
+    """
+    shift = shift % x.size(dim)
+    if shift == 0:
+        return x
+
+    left = x.narrow(dim, 0, x.size(dim) - shift)
+    right = x.narrow(dim, x.size(dim) - shift, shift)
+
+    return torch.cat((right, left), dim=dim)
+
+
+def roll(
+    x: torch.Tensor,
+    shift: List[int],
+    dim: List[int],
+) -> torch.Tensor:
+    """
+    Similar to np.roll but applies to PyTorch Tensors.
+
+    Args:
+        x: A PyTorch tensor.
+        shift: Amount to roll.
+        dim: Which dimension to roll.
+
+    Returns:
+        Rolled version of x.
+    """
+    if len(shift) != len(dim):
+        raise ValueError("len(shift) must match len(dim)")
+
+    for (s, d) in zip(shift, dim):
+        x = roll_one_dim(x, s, d)
+
+    return x
+
+
+def fftshift(x: torch.Tensor, dim: Optional[List[int]] = None) -> torch.Tensor:
+    """
+    Similar to np.fft.fftshift but applies to PyTorch Tensors
+
+    Args:
+        x: A PyTorch tensor.
+        dim: Which dimension to fftshift.
+
+    Returns:
+        fftshifted version of x.
+    """
+    if dim is None:
+        # this weird code is necessary for toch.jit.script typing
+        dim = [0] * (x.dim())
+        for i in range(1, x.dim()):
+            dim[i] = i
+
+    # also necessary for torch.jit.script
+    shift = [0] * len(dim)
+    for i, dim_num in enumerate(dim):
+        shift[i] = x.shape[dim_num] // 2
+
+    return roll(x, shift, dim)
+
+
+def ifftshift(x: torch.Tensor, dim: Optional[List[int]] = None) -> torch.Tensor:
+    """
+    Similar to np.fft.ifftshift but applies to PyTorch Tensors
+
+    Args:
+        x: A PyTorch tensor.
+        dim: Which dimension to ifftshift.
+
+    Returns:
+        ifftshifted version of x.
+    """
+    if dim is None:
+        # this weird code is necessary for toch.jit.script typing
+        dim = [0] * (x.dim())
+        for i in range(1, x.dim()):
+            dim[i] = i
+
+    # also necessary for torch.jit.script
+    shift = [0] * len(dim)
+    for i, dim_num in enumerate(dim):
+        shift[i] = (x.shape[dim_num] + 1) // 2
+
+    return roll(x, shift, dim)
 
 
 def main():
@@ -30,7 +188,7 @@ def main():
     filename = f'./samples/real/{args.task}/{fname}.npy'
     mask_filename = f'./samples/real/prospective/{fname}_mask.npy'
 
-    print('initaializing...')
+    print('initializing...')
     configs = importlib.import_module(f"configs.ve.fastmri_knee_320_ncsnpp_continuous")
     config = configs.get_config()
     img_size = config.data.image_size
@@ -38,6 +196,59 @@ def main():
 
     # Read data
     img = torch.from_numpy(np.load(filename))
+    print(img.type())
+    print(img.shape)
+
+    plt.imshow(np.log(np.abs(img)), cmap='gray')
+    plt.savefig('their_fftinputnp.png')
+    
+    file = "/vol/datasets/cil/2021_11_23_fastMRI_data/knee/unzipped/singlecoil_train/file1000898.h5"
+    hf = h5py.File(file, 'r')
+    print(hf['kspace'].shape)
+
+    k=hf['kspace'][0,hf['kspace'].shape[1]//2,:,:]
+    plt.imshow(np.log(np.abs(k)), cmap='gray')
+    plt.savefig('fftinputnp.png')
+
+    #torchvision.utils.save_image(np.log(np.abs(k.astype(np.float32))), 'fftnpinput.png')
+    print(k.dtype)
+    #kt = torch.from_numpy(k)
+    kt = torch.from_numpy(np.stack((k.real, k.imag), axis=-1))
+    print(kt.dtype)
+    #torchvision.utils.save_image(torch.log(kt.real), 'fftinput.png')
+    plt.imshow(torch.log(torch.abs(kt[:,:,0])).numpy(), cmap='gray')
+    plt.savefig('fftinput_.png')
+    ifftimg = ifft2c_new(kt)#torch.fft.ifft2(torch.fft.ifftshift(kt))#.unsqueeze(0)
+    print(ifftimg.shape)
+    #transform = T.Resize((320,320))
+    #fftimg = transform(fftimg).squeeze(0)
+    cropx = (hf['kspace'].shape[2]-320)//2
+    cropy = (hf['kspace'].shape[3]-320)//2 
+    ifftimg=ifftimg[cropx:cropx+320,cropy:cropy+320,:]
+    print(ifftimg.type())
+    print(ifftimg.shape)
+    plt.imshow(torch.abs(ifftimg[:,:,0]).numpy(), cmap='gray')
+    plt.savefig('ifftinput_.png')
+    img = ifftimg[:,:,0]
+    #torchvision.utils.save_image(torch.abs(fftimg)*255, 'ifftinput.png')
+    '''
+    img = fft2c_new(ifftimg)#torch.fft.fft2(torch.fft.fftshift(fftimg)) 
+    print(img.shape)
+    plt.clf()
+    plt.imshow(torch.log(torch.abs(img[:,:,0])).numpy(), cmap='gray')
+    plt.savefig('finalfftinput_.png')
+    print(img.type())
+
+    img = img[:,:,0]
+    print(img.type())
+    print(img.shape)
+    '''
+    #cropx = (hf['kspace'].shape[2]-320)//2
+    #cropy = (hf['kspace'].shape[3]-320)//2
+    #k=hf['kspace'][0,hf['kspace'].shape[1]//2,cropx:cropx+320,cropy:cropy+320]
+    #print(k.shape)
+    #img = torch.from_numpy(k)
+    print(img.shape)
     img = img.view(1, 1, 320, 320)
     img = img.to(config.device)
     if args.task == 'retrospective':
@@ -52,7 +263,7 @@ def main():
 
     ckpt_filename = f"./weights/checkpoint_95.pth"
     sde = VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=N)
-
+    
     config.training.batch_size = batch_size
     predictor = ReverseDiffusionPredictor
     corrector = LangevinCorrector
@@ -96,7 +307,8 @@ def main():
                                          save_root=save_root / 'recon_progress')
     # fft
     kspace = fft2(img)
-
+    #kspace = img
+    
     # undersampling
     under_kspace = kspace * mask
     under_img = torch.real(ifft2(under_kspace))
--- inference_single-coil.py
+++ inference_single-coil.py
@@ -14,7 +14,167 @@ from models.ema import ExponentialMovingAverage
 import matplotlib.pyplot as plt
 import importlib
 import argparse
+from typing import List, Optional
+import argparse
+import h5py
+import torchvision.transforms as T
+import torchvision
+import imageio
 
+
+
+def fft2c_new(data: torch.Tensor, norm: str = "ortho") -> torch.Tensor:
+    """
+    Apply centered 2 dimensional Fast Fourier Transform.
+
+    Args:
+        data: Complex valued input data containing at least 3 dimensions:
+            dimensions -3 & -2 are spatial dimensions and dimension -1 has size
+            2. All other dimensions are assumed to be batch dimensions.
+        norm: Normalization mode. See ``torch.fft.fft``.
+
+    Returns:
+        The FFT of the input.
+    """
+    if not data.shape[-1] == 2:
+        raise ValueError("Tensor does not have separate complex dim.")
+
+    data = ifftshift(data, dim=[-3, -2])
+    data = torch.view_as_real(
+        torch.fft.fftn(  # type: ignore
+            torch.view_as_complex(data), dim=(-2, -1), norm=norm
+        )
+    )
+    data = fftshift(data, dim=[-3, -2])
+
+    return data
+
+
+def ifft2c_new(data: torch.Tensor, norm: str = "ortho") -> torch.Tensor:
+    """
+    Apply centered 2-dimensional Inverse Fast Fourier Transform.
+
+    Args:
+        data: Complex valued input data containing at least 3 dimensions:
+            dimensions -3 & -2 are spatial dimensions and dimension -1 has size
+            2. All other dimensions are assumed to be batch dimensions.
+        norm: Normalization mode. See ``torch.fft.ifft``.
+
+    Returns:
+        The IFFT of the input.
+    """
+    if not data.shape[-1] == 2:
+        raise ValueError("Tensor does not have separate complex dim.")
+
+    data = ifftshift(data, dim=[-3, -2])
+    data = torch.view_as_real(
+        torch.fft.ifftn(  # type: ignore
+            torch.view_as_complex(data), dim=(-2, -1), norm=norm
+        )
+    )
+    data = fftshift(data, dim=[-3, -2])
+
+    return data
+
+
+# Helper functions
+
+
+def roll_one_dim(x: torch.Tensor, shift: int, dim: int) -> torch.Tensor:
+    """
+    Similar to roll but for only one dim.
+
+    Args:
+        x: A PyTorch tensor.
+        shift: Amount to roll.
+        dim: Which dimension to roll.
+
+    Returns:
+        Rolled version of x.
+    """
+    shift = shift % x.size(dim)
+    if shift == 0:
+        return x
+
+    left = x.narrow(dim, 0, x.size(dim) - shift)
+    right = x.narrow(dim, x.size(dim) - shift, shift)
+
+    return torch.cat((right, left), dim=dim)
+
+
+def roll(
+    x: torch.Tensor,
+    shift: List[int],
+    dim: List[int],
+) -> torch.Tensor:
+    """
+    Similar to np.roll but applies to PyTorch Tensors.
+
+    Args:
+        x: A PyTorch tensor.
+        shift: Amount to roll.
+        dim: Which dimension to roll.
+
+    Returns:
+        Rolled version of x.
+    """
+    if len(shift) != len(dim):
+        raise ValueError("len(shift) must match len(dim)")
+
+    for (s, d) in zip(shift, dim):
+        x = roll_one_dim(x, s, d)
+
+    return x
+
+
+def fftshift(x: torch.Tensor, dim: Optional[List[int]] = None) -> torch.Tensor:
+    """
+    Similar to np.fft.fftshift but applies to PyTorch Tensors
+
+    Args:
+        x: A PyTorch tensor.
+        dim: Which dimension to fftshift.
+
+    Returns:
+        fftshifted version of x.
+    """
+    if dim is None:
+        # this weird code is necessary for toch.jit.script typing
+        dim = [0] * (x.dim())
+        for i in range(1, x.dim()):
+            dim[i] = i
+
+    # also necessary for torch.jit.script
+    shift = [0] * len(dim)
+    for i, dim_num in enumerate(dim):
+        shift[i] = x.shape[dim_num] // 2
+
+    return roll(x, shift, dim)
+
+
+def ifftshift(x: torch.Tensor, dim: Optional[List[int]] = None) -> torch.Tensor:
+    """
+    Similar to np.fft.ifftshift but applies to PyTorch Tensors
+
+    Args:
+        x: A PyTorch tensor.
+        dim: Which dimension to ifftshift.
+
+    Returns:
+        ifftshifted version of x.
+    """
+    if dim is None:
+        # this weird code is necessary for toch.jit.script typing
+        dim = [0] * (x.dim())
+        for i in range(1, x.dim()):
+            dim[i] = i
+
+    # also necessary for torch.jit.script
+    shift = [0] * len(dim)
+    for i, dim_num in enumerate(dim):
+        shift[i] = (x.shape[dim_num] + 1) // 2
+
+    return roll(x, shift, dim)
 
 def main():
     ###############################################
@@ -36,6 +196,43 @@ def main():
 
     # Read data
     img = torch.from_numpy(np.load(filename).astype(np.complex64))
+
+    plt.clf()
+    plt.imshow(np.abs(img.real), cmap='gray')
+    plt.savefig('their_scfftinputnp.png')
+
+    #todo replace with data arg path
+    file = "/vol/datasets/cil/2021_11_23_fastMRI_data/knee/unzipped/singlecoil_train/file1000898.h5"
+    hf = h5py.File(file, 'r')
+    print(hf['kspace'].shape)
+
+    k=hf['kspace'][hf['kspace'].shape[0]//2,:,:]
+    plt.clf()
+    plt.imshow(np.log(np.abs(k)), cmap='gray')
+    plt.savefig('scfftinputnp.png')
+
+    kt = torch.from_numpy(np.stack((k.real, k.imag), axis=-1))
+    print(kt.dtype)
+    ifftimg = ifft2c_new(kt)#torch.fft.ifft2(torch.fft.ifftshift(kt))#.unsqueeze(0)
+    print(ifftimg.shape)
+    plt.clf()
+    plt.imshow(torch.abs(ifftimg[:,:,0]).numpy(), cmap='gray')
+    plt.savefig('scifftinput_.png')
+
+    cropx = (ifftimg.shape[0]-320)//2
+    cropy = (ifftimg.shape[1]-320)//2 
+    ifftimg=ifftimg[cropx:cropx+320,cropy:cropy+320,:]
+    print(ifftimg.shape)
+
+    plt.clf()
+    plt.imshow(torch.abs(ifftimg[:,:,0]).numpy(), cmap='gray')
+    plt.savefig('scifftinput_crop.png')
+
+    
+    #TODO use kspace directly and convert back with the functions here
+    #img.real = ifftimg[:,:,0]
+    #img.imag = ifftimg[:,:,1]
+
     img = img.view(1, 1, 320, 320)
     img = img.to(config.device)
 
