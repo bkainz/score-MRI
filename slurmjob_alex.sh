eb879843e7fece5f49149f39f0214c5b08ff46c9
Bernhard Kainz <b143dc12@alex1.nhr.fau.de>
2024-02-11 09:22:59 +0100

    alex slurm job

---
+++ slurmjob_alex.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --partition=a40
+#SBATCH --gres=gpu:a40:1
+#SBATCH --output=scoreMRI_%j.log
+#SBATCH --export=ALL
+#SBATCH --time=05:00:00  
+
+cd $WORK/score-MRI
+
+bash
+source ~/.bashrc
+source activate score-POCS 
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+srun python inference_multi-coil_SSOS.py --data '001' --N 3000 --acc_factor 32 --center_fraction 0.03
+
+#conda install nvidia/label/cuda-12.1.0::cuda-toolkit
+#conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+#pip install -r requirements.txt
+
+#srun --jobid=1083633 --overlap --pty /bin/bash
